dd0cfb13d4b7d16224bed948c4b24bdd42cd9248
Adam Fordyce <adamfordyce@me.com>
2018-02-12 23:00:40 +0000

    Changed column widths and bootstrap rules

--- index.php
+++ index.php
@@ -78,16 +78,16 @@ if (login_check($mysqli) == true) {
     <?php if ($logged == "in") { ?>
       <div class="row">
     <?php if (isset($_GET['error'])) { echo '<p class="error">Error Logging In!</p>'; } ?>
-        <div class="col-sm-0 col-lg-1"></div>
-        <div class="col-sm-12 col-lg-10" id="cal1" edit="<?php echo htmlentities($_SESSION['username']); ?>" uid="<?php echo htmlentities($_SESSION['user_id']);?>"></div>
-        <div class="col-sm-0 col-lg-1"></div>
+        <div class="col-sm-0 col-md-0 col-lg-1"></div>
+        <div class="col-sm-12 col-md-12 col-lg-10" id="cal1" edit="<?php echo htmlentities($_SESSION['username']); ?>" uid="<?php echo htmlentities($_SESSION['user_id']);?>"></div>
+        <div class="col-sm-0 col-md-0 col-lg-1"></div>
       </div>
     <?php } else { ?>
       <div class="row">
     <?php if (isset($_GET['error'])) { echo '<p class="error">Error Logging In!</p>'; } ?>
-        <div class="col-sm-0 col-lg-1"></div>
-        <div class="col-sm-12 col-lg-10" id="calnoedit"></div>
-        <div class="col-sm-0 col-lg-1"></div>
+        <div class="col-sm-0 col-md-0 col-lg-1"></div>
+        <div class="col-sm-12 col-md-12 col-lg-10" id="calnoedit"></div>
+        <div class="col-sm-0 col-md-0 col-lg-1"></div>
       </div>
     <?php } ?>
     </div>
